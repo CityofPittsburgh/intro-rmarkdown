c8e997f8db1821d178904015efee18f68c55dbf5
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2019-04-05 12:36:37 -0400

    Markdown only packages

--- 00-welcome/packages.R
+++ 00-welcome/packages.R
@@ -1,22 +1,16 @@
 # Install packages that will be used in the workshop
-from_cran <- c("shiny", "rmarkdown", 
-               "DT", "devtools", "flexdashboard", "gapminder",
-               "rticles", "shinydashboard", "shinythemes", 
-               "tidyverse", "tufte", "xaringan")
+from_cran <- c("rmarkdown", "DT", "devtools", "gapminder",
+               "rticles", "tidyverse", "tufte", "xaringan")
 
 install.packages(from_cran, repos = "http://cran.rstudio.com")
 
 # Load packages
 
-library(shiny)
 library(rmarkdown)
 library(DT)
 library(devtools)
-library(flexdashboard)
 library(gapminder)
 library(rticles)
-library(shinydashboard)
-library(shinythemes)
 library(tidyverse)
 library(tufte)
 library(xaringan)
